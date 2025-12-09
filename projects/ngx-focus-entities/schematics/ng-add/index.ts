dfa130ac1aa90659c5bdb01f04b2218a1333f54b
Gildéric Deruette <gilderic.deruette@kleegroup.com>
2025-12-09 12:11:05 +0100

    V21.0.3 : update schematics

--- projects/ngx-focus-entities/schematics/ng-add/index.ts
+++ projects/ngx-focus-entities/schematics/ng-add/index.ts
@@ -1,10 +1,16 @@
 import { Rule } from '@angular-devkit/schematics';
-import { addDependency } from '@schematics/angular/utility';
+import { addDependency, ExistingBehavior } from '@schematics/angular/utility';
 import { Schema } from './schema';
-export function ngAdd(_options: Schema): Rule[] {
-  return [
-    addDependency('ngx-focus-entities', require('../../package.json').version),
-    addDependency('zod', '^4'),
-    addDependency('@focus4/entities', '^12'),
-  ];
+export function ngAdd(_options: Schema): Rule {
+  return (tree, context) => {
+    addDependency('ngx-focus-entities', require('../../package.json').version, {
+      existing: ExistingBehavior.Skip,
+    })(tree, context);
+    addDependency('zod', '^4', {
+      existing: ExistingBehavior.Skip,
+    })(tree, context);
+    addDependency('@focus4/entities', '^12', {
+      existing: ExistingBehavior.Skip,
+    })(tree, context);
+  };
 }
