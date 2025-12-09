c83c77005c497a3d51552e6001b3bf7192326852
Gildéric Deruette <gilderic.deruette@kleegroup.com>
2025-12-09 11:54:36 +0100

    Add zod & @focus4/entities dependencies to ng(add schematics

--- projects/ngx-focus-entities/schematics/ng-add/index.ts
+++ projects/ngx-focus-entities/schematics/ng-add/index.ts
@@ -1,8 +1,10 @@
 import { Rule } from '@angular-devkit/schematics';
-import {
-  addDependency
-} from '@schematics/angular/utility';
+import { addDependency } from '@schematics/angular/utility';
 import { Schema } from './schema';
-export function ngAdd(_options: Schema): Rule {
-  return addDependency('ngx-focus-entities', require("../../package.json").version);
+export function ngAdd(_options: Schema): Rule[] {
+  return [
+    addDependency('ngx-focus-entities', require('../../package.json').version),
+    addDependency('zod', '^4'),
+    addDependency('@focus4/entities', '^12'),
+  ];
 }
